52886cdec3e0ac5f2053b1d827c5edcafe6003e9
Leandro Sousa <leandrosousa69outlook.com>
2025-01-08 15:45:27 -0300

    routes fix 2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router"
+import { BrowserRouter, Routes, Route } from "react-router"
 import { PokeList } from '../pages/pokeList'
 import { Poke } from '../pages/poke'
 
